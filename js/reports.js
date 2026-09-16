168e409fe26420a4bc045cb3763a5013d05defd9
mediafocolarepak <https://github.com/mediafocolarepak@gmail.com>
2026-09-17 01:11:48 +0500

    Add inline Age group column to Users grid, next to Full name

--- js/reports.js
+++ js/reports.js
@@ -1,4 +1,4 @@
-import { sb, State, esc, labelOf, optionsHtml, withStatus, withStatusCount } from './core.js?v=20260917005437';
+import { sb, State, esc, labelOf, optionsHtml, withStatus, withStatusCount } from './core.js?v=20260917011147';
 
 export function renderReportsView(main) {
   main.innerHTML = `
